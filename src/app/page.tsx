7fe07c72b2286290c0db2531ab7c4d513acf37d7
Hamdullah <hamdullahs046@gmail.com>
2024-10-01 00:16:45 +0500

    add image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import img from '../images/Screenshot 2024-09-30 233904.png'
 
 export default function Home() {
   return (
     <>
-    <h1>Hello </h1>
+    <h1>Hello </h1> 
+    <Image src={img} alt=" hell" />
     </>
   );
 }
